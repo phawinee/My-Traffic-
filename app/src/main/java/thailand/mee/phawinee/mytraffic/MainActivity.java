98ac6b25b58b2828ab2829e42c6b9a10153d082b
takkatan <phawinee.tak@gmail.com>
2016-07-02 15:00:29 +0700

    ไว้ต่อพรุ่งนี้

--- app/src/main/java/thailand/mee/phawinee/mytraffic/MainActivity.java
+++ app/src/main/java/thailand/mee/phawinee/mytraffic/MainActivity.java
@@ -1,13 +1,49 @@
 package thailand.mee.phawinee.mytraffic;
 
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+    //Explicit การประกาศตัวแปร
+    private ListView trafficListView;
+    private Button aboutButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-}
+
+        //Bind widdet
+        trafficListView = (ListView) findViewById(R.id.listView);
+        aboutButton = (Button) findViewById(R.id.button);
+
+        //controller for button
+        aboutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                //sound effect
+                MediaPlayer mediaPlayer = MediaPlayer.create(getBaseContext(),R.raw.dog);
+                mediaPlayer.start();
+                //web view
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse("https://youtu.be/kHSFpGBFGHY"));
+                startActivity(intent);
+
+
+
+
+            }   //onclick
+        });
+
+
+    }   //main methood
+}   //main Class นี่คือคลาสหลัก
+
